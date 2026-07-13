cd1df59f71f6b8f52912f1092171da0b30bdc435
ismagarri <ismagarri@hotmail.com>
2026-07-12 19:41:07 -0600

    Actualización y extensión de código de los archivos

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
+/* 
+  ARCHIVO: app.js
+  DESCRIPCIÓN: Lógica central del tutorial.
+  Contiene la interactividad del DOM, animaciones y peticiones asíncronas AJAX.
+*/
+
 $(document).ready(function() {
-    $('#btnCargar').click(function() {
+
+    // 1. Funcionalidad Extra: Interacción básica de la página de Inicio
+    // Utilizamos fadeToggle para demostrar animaciones de jQuery
+    $('#btnInteraccion').click(function() {
+        $('#mensajeInteraccion').fadeToggle('slow');
+    });
+
+    // 2. Escenario 1: Petición AJAX para obtener Usuarios
+    $('#btnCargarUsuarios').click(function() {
         $.ajax({
             url: 'https://jsonplaceholder.typicode.com/users',
             method: 'GET',
@@ -9,11 +23,31 @@ $(document).ready(function() {
                     html += '<li>' + user.name + ' - ' + user.email + '</li>';
                 });
                 html += '</ul>';
-                $('#resultado').html(html);
+                $('#resultadoUsuarios').html(html);
             },
             error: function() {
-                $('#resultado').html('<p>Error al cargar los datos.</p>');
+                $('#resultadoUsuarios').html('<p>Error al cargar usuarios.</p>');
             }
         });
     });
+
+    // 3. Escenario 2: Petición AJAX para obtener Posts (con parámetro de filtro)
+    $('#btnCargarPosts').click(function() {
+        $.ajax({
+            url: 'https://jsonplaceholder.typicode.com/posts?_limit=5',
+            method: 'GET',
+            success: function(data) {
+                let html = '<h3>Últimos Posts:</h3><ul>';
+                $.each(data, function(index, post) {
+                    html += '<li><strong>' + post.title + '</strong></li>';
+                });
+                html += '</ul>';
+                $('#resultadoPosts').html(html);
+            },
+            error: function() {
+                $('#resultadoPosts').html('<p>Error al cargar posts.</p>');
+            }
+        });
+    });
+
 });
